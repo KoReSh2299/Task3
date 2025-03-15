d71184db063e86849e4d3d6980f1a5ba85ec95a4
KoReSh <sasha.gotchenya@mail.ru>
2025-03-15 11:04:59 +0300

    Добавьте файлы проекта.

---
+++ StazhaTask3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StazhaTask3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
